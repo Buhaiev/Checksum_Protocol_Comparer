8a04d056481c5c4b53b38662e35463f5d3ad9577
Buhaiev <36414220+Buhaiev@users.noreply.github.com>
2021-10-02 15:46:28 +0300

    Add files via upload

--- Checksum Protocol Comparison.cpp
+++ Checksum Protocol Comparison.cpp
@@ -1,64 +1,45 @@
 ﻿/*The point of this project is to compare data checking protocols used in the Internet*/
 #include <iostream>
 #include "methods.h"
+//typedef std::pair< std::function< std::vector<bool>(std::vector<bool>)>, std::function< std::array<int, 5>(std::vector<bool>, std::vector<bool>)>> functionPair;
+functionPair parityBit = { parityBitEncode, parityBitCheck };
+functionPair doubleParityBit = { doubleParityBitEncode, doubleParityBitCheck};
+functionPair checksum16 = { checksumEncode16, checksumCheck16 };
+functionPair doubleSend = { doubleSendEncode, doubleSendCheck };
+functionPair tripleSend = { tripleSendEncode, tripleSendCheck };
+functionPair crc32 = { crcEncode32, crcCheck32 };
+
+std::vector<bool> getVectFromStr(std::string str) {
+	std::vector<bool> res;
+	for (int i = 0; i < str.size(); i++) {
+		res.push_back(int(str[i])-48);
+	}
+	return res;
+}
 
 int main()
 {
-	/*for (int i = 0; i < 10; i++) {
-		std::string text = generateText();
-		std::cout << text << "	";
-		std::vector<bool> binText = textToBinary(text);
-		for (int i = 0; i < binText.size(); i++) {
-			if (((i) % 8 == 0)&&(i!=0)) std::cout << ".";
-			std::cout << binText[i];
-		}
-		std::string renewedText = binaryToText(binText);
-		std::cout << "	" << renewedText;
-		std::cout << std::endl;
-	}*/
-	std::array<int, 5> totalResult = { 0 };
-	for (int i = 0; i < 20; i++) {
-		std::string text = generateText();
-		std::cout << text << std::endl;
-		std::vector<bool> binText = textToBinary(text);
-		for (int i = 0; i < binText.size(); i++) {
-			if (((i) % 8 == 0) && (i != 0)) std::cout << ".";
-			std::cout << binText[i];
-		}
-		std::cout << "	" << "Simple" << std::endl;
-
-		std::vector<bool> protectedText = parityBit(binText);
-		for (int i = 0; i < protectedText.size(); i++) {
-			if (((i) % 8 == 0) && (i != 0)) std::cout << ".";
-			std::cout << protectedText[i];
-		}
-		std::cout << "	" << "Protected" << std::endl;
-
-		std::vector<bool> corruptedText = simpleCorrupt(protectedText);
-		for (int i = 0; i < corruptedText.size(); i++) {
-			if (((i) % 8 == 0) && (i != 0)) std::cout << ".";
-			std::cout << corruptedText[i];
-		}
-		std::cout << "	"<<"Corrupt"<<std::endl;
-
-		std::array<int, 5> result = checkParityBit(corruptedText, binText);
+	srand(time(NULL));
+//#ifdef _DEBUG
+	auto allStart = std::chrono::steady_clock::now();
+//#endif
+	std::array<long long int, 5> totalResult = { 0 };
+	for (int i = 0; i < NUMBER_OF_DATAGRAMS; i++) {
+		std::array<long long int, 5> result = checkEfficiency(tripleSend);
 		for (int i = 0; i < 5; i++) {
 			totalResult[i] += result[i];
 		}
-		std::cout << "Bytes" <<"    "<< result[0] << std::endl;
-		std::cout << "True Positive" << "    " << result[1] << std::endl;
-		std::cout << "True Negative" << "    " << result[2] << std::endl;
-		std::cout << "False Positive" << "    " << result[3] << std::endl;
-		std::cout << "False Negative" << "    " << result[4] << std::endl;
-		//Text renewal for later or just not needed
-		/*std::string renewedText = binaryToText(corruptedText);
-		std::cout << renewedText;
-		std::cout << std::endl;
-		std::cout << std::endl;*/
 	}
 	std::cout << "Total bytes" << "    " << totalResult[0] << std::endl;
-	std::cout << "Total True Positive" << "    " << totalResult[1] << std::endl;
-	std::cout << "Total True Negative" << "    " << totalResult[2] << std::endl;
-	std::cout << "Total False Positive" << "    " << totalResult[3] << std::endl;
-	std::cout << "Total False Negative" << "    " << totalResult[4] << std::endl;
+	std::cout << "Total True Positive" << "    " << totalResult[1] << "		Persentage: " <<  double(totalResult[1]) / NUMBER_OF_DATAGRAMS*100 << std::endl;
+	std::cout << "Total True Negative" << "    " << totalResult[2] << "		Persentage: " << double(totalResult[2]) / NUMBER_OF_DATAGRAMS * 100 << std::endl;
+	std::cout << "Total False Positive" << "    " << totalResult[3] << "		Persentage: " << double(totalResult[3]) / NUMBER_OF_DATAGRAMS * 100 << std::endl;
+	std::cout << "Total False Negative" << "    " << totalResult[4] << "		Persentage: " << double(totalResult[4]) / NUMBER_OF_DATAGRAMS * 100 << std::endl;
+
+//#ifdef _DEBUG
+	auto allEnd = std::chrono::steady_clock::now();
+	auto elSec = std::chrono::duration_cast<std::chrono::milliseconds>( allEnd - allStart);
+	std::cout << "	" << elSec.count() << std::endl;
+//#endif
 }
+
--- methods.h
+++ methods.h
@@ -5,23 +5,47 @@
 #include <vector>
 #include <random>
 #include <array>
-//Constants
-const int MAX_TEXT_SIZE = 50;
-const int CORRUPT_CHANCE = 25;
+#include <functional>
+#include <time.h>
+#include <chrono>
+//Constants, typedefs, etc.
+//A pair of functions: one encodes, the other one check efficiency
+const int MAX_TEXT_SIZE =1500;
+const int NUMBER_OF_DATAGRAMS = 1'000;
+typedef std::pair<std::function< std::vector<bool>(std::vector<bool>)>, std::function< std::array<long long int, 5>(std::vector<bool>, std::vector<bool>)>> functionPair;
+const int CORRUPT_CHANCE = 1'500'000; //1 error in CORRUPT_CHANCE bits
 //Functions 
-std::vector<bool> simpleCorrupt(std::vector<bool> text);
-std::string generateText();
-std::vector<bool> textToBinary(std::string text);
-std::string binaryToText(std::vector<bool> binText);
-double checkEfficiency();
+std::vector<bool> simpleCorrupt(std::vector<bool>text);
+std::vector<bool> specialCorrupt(std::vector<bool> textToCorrupt);
+
+//std::string generateText(int size, bool random);
+//std::vector<bool> textToBinary(std::string text);
+//std::string binaryToText(std::vector<bool> binText);
+std::vector<bool> generateBinaryText();
+std::array<long long int, 5> checkEfficiency(functionPair funcs);
+
 //A list of protocols for comparison
 
- /*ASCII symbols are 7-bit long, and we have 1 spare bit. We can use it as a method to check the correctness!
- The 1st bit in each byte is now a parity bit; it is set so, that the sum of bits with it is even*/
-std::vector<bool> parityBit(std::vector<bool> textToProtect);  
-std::array<int,5> checkParityBit(std::vector<bool> protectedAndCorrupted, std::vector<bool> trueText);
-/*Answer structure: 
-Total byte number, True Positive(correct transmitted and correct accroding to parity bit),
-True Negative(wrongly transmitted, mistake noticed by parity bit), False Positive(wrongly transmitted, mistake not noticed), 
-False Negative(correctly transmitted, but incorrectly marked as wrong)
-*/
+ /*Answer structure:
+ Total byte number, True Positive(correct transmitted and correct accroding to parity bit),
+ True Negative(wrongly transmitted, mistake noticed by parity bit), False Positive(wrongly transmitted, mistake not noticed),
+ False Negative(correctly transmitted, but incorrectly marked as wrong)
+ That refers to whole datagrams.
+ */
+std::vector<bool> parityBitEncode(std::vector<bool> textToProtect);
+std::array<long long int,5> parityBitCheck(std::vector<bool> protectedAndCorrupted, std::vector<bool> trueText);
+
+std::vector<bool>  doubleParityBitEncode(std::vector<bool> textToProtect);
+std::array<long long int, 5> doubleParityBitCheck(std::vector<bool>  protectedAndCorrupted, std::vector<bool> trueText);
+
+std::vector<bool>  checksumEncode16(std::vector<bool> textToProtect);
+std::array<long long int, 5> checksumCheck16(std::vector<bool>  protectedAndCorrupted, std::vector<bool> trueText);
+
+std::vector<bool> doubleSendEncode(std::vector<bool> textToProtect);
+std::array<long long int, 5> doubleSendCheck(std::vector<bool>  protectedAndCorrupted, std::vector<bool> trueText);
+
+std::vector<bool> tripleSendEncode(std::vector<bool> textToProtect);
+std::array<long long int, 5> tripleSendCheck(std::vector<bool>  protectedAndCorrupted, std::vector<bool> trueText);
+
+std::vector<bool> crcEncode32(std::vector<bool> textToProtect);
+std::array<long long int, 5> crcCheck32(std::vector<bool>  protectedAndCorrupted, std::vector<bool> trueText);
